b0e96b886ba014309f419f540e932eaa716606a2
eric-moreno <emoreno@mit.edu>
2023-11-16 15:25:48 -0600

    local script changes

--- local_scripts/plotPhi.sh
+++ local_scripts/plotPhi.sh
@@ -17,8 +17,10 @@ for mass in ${masslist[@]}; do
     dirname=$1_$2_m${mass}
     mkdir -p ${dirname}/
     mkdir -p $1_$2/
-    scp cmslpc-sl7.fnal.gov:/uscms_data/d3/drankin/HTauTau/CMSSW_10_2_13/src/boostedhiggs/${1}/${2}/{full,had*}Model_m${mass}/*{Model.root,pdf} ${dirname}/
-    scp cmslpc-sl7.fnal.gov:/uscms_data/d3/drankin/HTauTau/CMSSW_10_2_13/src/boostedhiggs/${1}/${2}/limit*pdf ${1}_${2}/
+
+
+    scp /uscms_data/d3/eamoreno/Analysis/phitautau/CMSSW_10_2_13/src/phitautau-fitcode/${1}/${2}/{full,had*}Model_m${mass}/*{Model.root,pdf} ${dirname}/
+    scp /uscms_data/d3/eamoreno/Analysis/phitautau/CMSSW_10_2_13/src/phitautau-fitcode/${1}/${2}/limit*pdf ${1}_${2}/
   else
     dirname=$1_m${mass}
     mkdir -p ${dirname}/
@@ -29,28 +31,34 @@ for mass in ${masslist[@]}; do
 
   for cat in ${modellist[@]}; do
     if [[ ${cat} == "hadhad" ]]; then
-      prelist=(ptbin0 ptbin1 wlnuCR topCR)
+      prelist=(SR wlnuCR topCR)
+      #prelist=(SR)
     elif [[ ${cat} == "limit" ]]; then
       break
     else
-      prelist=(ptbin0 ptbin1 wlnuCR topCR)
+      prelist=(SR wlnuCR topCR)
+      #prelist=(SR)
     fi
     for prefix in ${prelist[@]}; do
       for ptype in prefit fit_b fit_s; do
         if [[ ${cat} == "full" ]]; then
           for chan in hadhad hadel hadmu; do
-            if [[ ${prefix} == "ptbin0" || ${prefix} == "ptbin1" ]]; then
-              python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${chan}${year} ${prefix}loosepass${chan}${year}${year} --label ${cat}_${chan}_${ptype}_${year}
+            if [ ${prefix} == "SR" ]; then
+              python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions fail${chan}${year} loosepass${chan}${year}${year} --label ${cat}_${chan}_${ptype}_${year} --prefix ${prefix} --channel ${chan}
+              #python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${chan}${year} ${prefix}loosepass${chan}${year}${year} --label ${cat}_${chan}_${ptype}_${year}
             else
-              python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${chan}${year} ${prefix}loosepass${chan}${year}${year} ${prefix}pass${chan}${year} --label ${cat}_${chan}_${ptype}_${year}
+              python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${chan}${year} ${prefix}loosepass${chan}${year}${year} ${prefix}pass${chan}${year} --label ${cat}_${chan}_${ptype}_${year} --prefix ${prefix} --channel ${chan}
             fi
           done
         else
-          if [[ ${prefix} == "ptbin0" || ${prefix} == "ptbin1" ]]; then
-            python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${cat}${year} ${prefix}loosepass${cat}${year} --label ${cat}_${ptype}_${year}
+          if [ ${prefix} == "SR" ]; then
+            echo ptype ${ptype}
+            python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions fail${cat}${year} loosepass${cat}${year} --label ${cat}_${ptype}_${year} --prefix ${prefix} --channel ${cat}
+            #python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${cat}${year} ${prefix}loosepass${cat}${year} --label ${cat}_${ptype}_${year}
           else
-            python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${cat}${year} ${prefix}loosepass${cat}${year} ${prefix}pass${cat}${year} --label ${cat}_${ptype}_${year}
+            python quick_distplot_phi.py --infile fitDiagnostics.${cat}Model.root --tree shapes_${ptype} --regions ${prefix}fail${cat}${year} ${prefix}loosepass${cat}${year} ${prefix}pass${cat}${year} --label ${cat}_${ptype}_${year} --prefix ${prefix} --channel ${cat}
           fi
+          
         fi
       done
     done
--- local_scripts/quick_distplot_phi.py
+++ local_scripts/quick_distplot_phi.py
@@ -1,6 +1,7 @@
 #import CombineHarvester.CombineTools.plotting as plot
 import ROOT
 import argparse
+import os
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
@@ -10,6 +11,8 @@ parser.add_argument('--infile',      dest='infile',      default="infile.root", 
 parser.add_argument('--tree',        dest='tree',        default="shapes_prefit",     help="tree")
 parser.add_argument('--regions',     dest='regions',     default=["ptbin0pass"],      help="regions",   nargs='+')
 parser.add_argument('--label',       dest='label',       default="prefit_pt0pass",    help="label")
+parser.add_argument('--prefix',      dest='prefix',      default="topCR",             help="control or signal region")
+parser.add_argument('--channel',      dest='channel',      default="hadel",             help="channel hadhad, hadel, or hadmu")
 args = parser.parse_args()
 
 #python quick_distplot.py --infile fitDiagnostics.hadmuModel.root --tree shapes_prefit --regions ptbin0fail ptbin0loosepass ptbin0pass ptbin1fail ptbin1loosepass ptbin1pass topCRfail topCRloosepass topCRpass wlnuCRfail wlnuCRloosepass wlnuCRpass --label hadmu_prefit
@@ -23,13 +26,12 @@ pad1 = ROOT.TPad( 'pad1', 'plot',  0.05, 0.35, 0.95, 0.95 )
 pad2 = ROOT.TPad( 'pad2', 'ratio', 0.05, 0.05, 0.95, 0.35 )
 pad1.Draw()
 pad2.Draw()
-
 fin = ROOT.TFile(args.infile)
 
 first_dir = args.tree
 for second_dir in args.regions:
     pad1.cd()
-    
+    print(second_dir)
     h_err = fin.Get(first_dir + '/' + second_dir + '/total_background')
     h_top = fin.Get(first_dir + '/' + second_dir + '/top')
     h_wlnu = fin.Get(first_dir + '/' + second_dir + '/wlnu')
@@ -39,7 +41,7 @@ for second_dir in args.regions:
     h_h125 = fin.Get(first_dir + '/' + second_dir + '/htt125')
     h_sig = fin.Get(first_dir + '/' + second_dir + '/total_signal')
     h_dat = fin.Get(first_dir + '/' + second_dir + '/data')  # This is a TGraphAsymmErrors, not a TH1F
-    
+    print(h_err, h_top, h_wlnu, h_ztt, h_vvqq, h_qcd, h_h125, h_sig, h_dat)
     h_all = ROOT.THStack("h_all","h_all");
     atleast1 = False
     if str(type(h_qcd))=='<class \'ROOT.TH1F\'>':
@@ -91,7 +93,7 @@ for second_dir in args.regions:
     elif (first_dir=="shapes_fit_s"): 
         title_str = "S+B fit, ";
     h_all.SetTitle(title_str+second_dir);
-
+    print(title_str)
     h_all.Draw('HIST')
     #h_all.GetXaxis().SetTitle("m_{NN}")
     h_all.GetYaxis().SetTitle("Events / GeV")
@@ -195,8 +197,17 @@ for second_dir in args.regions:
     h_line.GetYaxis().SetRangeUser(-3.,3.)
     h_line.GetXaxis().SetTitle("m_{NN}")
 
-    canvas.SaveAs('fitplot_%s%s.pdf'%(second_dir,args.label))
+    if not os.path.exists(args.prefix):
+    # If it doesn't exist, then create it
+        os.mkdir(args.prefix)
+
+    if not os.path.exists(os.path.join(args.prefix, args.channel)):
+    # If it doesn't exist, then create it
+        os.mkdir(os.path.join(args.prefix, args.channel))
+
+    canvas.SaveAs('%s/%s/fitplot_%s%s.pdf'%(args.prefix, args.channel, second_dir, args.label))
     pad1.SetLogy()
-    canvas.SaveAs('fitplot_%s%s_logy.pdf'%(second_dir,args.label))
+
+    canvas.SaveAs('%s/%s/fitplot_%s%s_logy.pdf'%(args.prefix, args.channel, second_dir,args.label))
     pad1.SetLogy(0)
     #canvas.SaveAs('fitplot_%s%s.png'%(second_dir,args.label))
